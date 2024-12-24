00d5ed984d5476f8641c4dd5036a153ae17cc54b
sekaha <davidsommerfield.zg@gmail.com>
2024-12-24 15:43:16 -0600

    puntuation

--- Graph_Utils/sfs_sfb_stack_bars.py
+++ Graph_Utils/sfs_sfb_stack_bars.py
@@ -1,12 +1,14 @@
+from matplotlib.colors import LinearSegmentedColormap
 import matplotlib.pyplot as plt
 from collections import defaultdict
 from conversion import merge, rgb_to_hex, okhsl_to_srgb, oklab_to_linear_srgb, linear_srgb_to_oklab, srgb_to_okhsl
 from math import log10
 
-high_col = srgb_to_okhsl(252, 231, 44)
+high_h, high_s, high_l = srgb_to_okhsl(252, 231, 44)
+high_col = (high_h, high_s, high_l*0.97)
 low_col = srgb_to_okhsl(67, 13, 85)
 
-chars = "etaoinsrhldcumfgpywbvkxjzq"
+chars = "etaoinsrhldcumfgpywbvkxjzq;'-,./"
 
 # get bigram freqs
 bigram_freqs = defaultdict(int)
@@ -14,10 +16,13 @@ bigram_freqs = defaultdict(int)
 with open('bigrams.txt') as f:
     for l in f:
         bigram, freq = l.split('\t')
+        bigram = bigram.lower()
 
-        if bigram.isalpha():
+        if all(c in chars for c in bigram):
             bigram_freqs[bigram] += int(freq)
             bigram_freqs[bigram[1]+bigram[0]] += int(freq)
+
+print(bigram_freqs['-e'])
 
 # get skipgram freqs
 skipgram_freqs = defaultdict(int)
@@ -25,12 +30,13 @@ skipgram_freqs = defaultdict(int)
 with open('1-skip.txt') as f:
     for l in f:
         skipgram, freq = l.split('\t')
+        skipgram = skipgram.lower()
 
-        if skipgram.isalpha():
+        if all(c in chars for c in skipgram):
             skipgram_freqs[skipgram] += int(freq)
             skipgram_freqs[skipgram[1]+skipgram[0]] += int(freq)
 
-high_f = max(*skipgram_freqs.values(), *bigram_freqs.values())
+high_f = max(*skipgram_freqs.values(), *bigram_freqs.values())+10000
 high_scale = max(bigram_freqs[c1+c2]+skipgram_freqs[c1+c2] for c1 in chars for c2 in chars)*1
 
 def get_col(freq, offset=0):
@@ -38,7 +44,10 @@ def get_col(freq, offset=0):
 
     h, s, l = merge(low_col, high_col, t)
 
-    return rgb_to_hex(*okhsl_to_srgb(h, s, min(l+offset, 0.95)))
+    return rgb_to_hex(*okhsl_to_srgb(h, s, min(l+offset, 1)))
+
+bigram_cmap = LinearSegmentedColormap.from_list("custom", [get_col(v, 0.09) for v in range(0, high_f, 20000)])
+# skipgram_cmap = LinearSegmentedColormap.from_list("custom",  [get_col(v, -0.05) for v in range(0, high_f, 20000)])
 
 # Chart
 for target in chars:
@@ -47,10 +56,10 @@ for target in chars:
     indices = sorted(range(len(labels)), key=lambda i: -(bigram_freqs[labels[i]])) # skipgram_freqs[labels[i]]+
     labels = [labels[i] for i in indices]
 
-    bigram_colors = [get_col(bigram_freqs[l], 0.2) for l in labels]
-    skipgram_colors = [get_col(skipgram_freqs[l], -0.05) for l in labels]
+    bigram_colors = [get_col(bigram_freqs[l], 0.09) for l in labels]
+    skipgram_colors = [get_col(skipgram_freqs[l], -0.09) for l in labels]
 
-    fig, ax = plt.subplots(figsize=(18, 9))
+    fig, ax = plt.subplots(figsize=(19, 9))
     # ax.set_ylim(0, high_scale) # 10**int(log10(high_scale)+0.5)
 
     # Plot each bar type
@@ -59,11 +68,20 @@ for target in chars:
     bigram_bar = ax.bar(labels, bigram_vals, label='bigram',  color=bigram_colors, edgecolor='white')
     skipgram_bar = ax.bar(labels, skipgram_vals, label='skipgram', bottom=bigram_vals, color=skipgram_colors, edgecolor='white')
 
-    plt.ylabel('Occurence Count', fontsize=24)
+    plt.ylabel('Cummulitative Occurence Count', fontsize=18)
     # plt.xlabel('Skipgram', fontsize=24)
-    plt.title(f"{target.upper()} - Bigram (Bottom) vs. Skipgram (Top) Occurences", fontsize=32)
-    plt.xticks(fontsize=20)
-    plt.yticks(fontsize=20)
+    plt.title(f"{target.upper()} - Bigram (Bottom) vs. Skipgram (Top) Occurences", fontsize=24)
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+
+    # Gradient legend explanation
+    bigram_sm = plt.cm.ScalarMappable(cmap=bigram_cmap, norm=plt.Normalize(vmin=0, vmax=high_f))
+    bigram_cbar = plt.colorbar(bigram_sm)
+    bigram_cbar.ax.set_ylabel('Frequency Intensity', fontsize=18, rotation=270, labelpad=20)
+
+    #skipgram_sm = plt.cm.ScalarMappable(cmap=skipgram_cmap, norm=plt.Normalize(vmin=0, vmax=high_f))
+    #skipgram_cbar = plt.colorbar(skipgram_sm)
+    # skipgram_cbar.ax.set_ylabel('Skipgram Frequency Intensity', fontsize=16)
 
     
     plt.savefig(f'Graph_Utils/out/{target.upper()}_Skipgram_Bars.png', format='png', dpi=300)
